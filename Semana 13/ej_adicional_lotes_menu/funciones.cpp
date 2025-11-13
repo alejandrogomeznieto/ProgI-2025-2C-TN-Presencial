6ccb8990690476be7a28c2d165d97cd9635b0066
José Alberto Rodriguez <69548242+Albertstone98@users.noreply.github.com>
2025-11-13 18:09:09 -0300

    UPDATE
    Menú + Puntos

--- Semana 13/ej_adicional_lotes_menu/funciones.cpp
+++ Semana 13/ej_adicional_lotes_menu/funciones.cpp
@@ -10,12 +10,26 @@ void menuPrincipal()
     bool salir = false;
     int opcion;
 
+    /// VECTORES STRUCT
+    const int CANT_AEROLINEA = 2;
+    Aerolinea vAerolineas[CANT_AEROLINEA];
+    const int CANT_VUELOS = 3;
+    Vuelo vVuelos[CANT_VUELOS];
+
+    /// VALIDADORES
+    bool seCargaronAerolineas = false;
+    bool seCargaronVuelos = false;
+    bool seProceso = false;
+
+    // PuntoB
+    float vecRecDestinos[20]{};
+
     do
     {
         system("cls"); //Limpio la Pantalla de la Consola
 
         opcion = mostrarOpciones();
-        analizarOpcion(opcion, salir);
+        analizarOpcion(opcion, salir, vAerolineas, CANT_AEROLINEA, vVuelos, CANT_VUELOS, seCargaronAerolineas, seCargaronVuelos, seProceso, vecRecDestinos);
 
         system("pause");
     }
@@ -41,21 +55,37 @@ int mostrarOpciones()
     return opc;
 }
 
-void analizarOpcion(int opcion, bool& salir)
+void analizarOpcion(int opcion, bool& salir, Aerolinea vecAero[], int tam1, Vuelo vecVuelo[], int tam2, bool &lote1, bool &lote2, bool &lote3, float vecDestinos[20])
 {
+    system("cls");
     switch(opcion)
     {
     case 1:
-        cout << "OPCIÓN 1" << endl;
+        lote1 = cargarAerolineas(vecAero, tam1);
         break;
     case 2:
-        cout << "OPCIÓN 2" << endl;
+        if(lote1){
+            lote2 = cargarVuelos(vecVuelo, tam2);
+        }
+        else{
+            cout << "Todavía no se cargaron las Aerolíneas!" << endl;
+        }
         break;
     case 3:
-        cout << "OPCIÓN 3" << endl;
+        if(lote1 && lote2){
+            lote3 = procesarReservas(vecAero, tam1, vecVuelo, tam2, vecDestinos);
+        }
+        else{
+            cout << "Todavía no se cargaron los Lotes de Carga!" << endl;
+        }
         break;
     case 4:
-        menuReportes();
+        if(lote3){
+            menuReportes(vecAero, tam1, vecDestinos);
+        }
+        else{
+            cout << "Todavía no se cargaron las Ventas!" << endl;
+        }
         break;
     case 0:
         cout << "Hasta la Vista, Baby!!!" << endl;
@@ -66,7 +96,7 @@ void analizarOpcion(int opcion, bool& salir)
     }
 }
 
-void menuReportes()
+void menuReportes(Aerolinea vAero[], int tam1, float vecDestinos[20])
 {
     bool volver = false;
     int opcion;
@@ -76,7 +106,7 @@ void menuReportes()
         system("cls"); //Limpio la Pantalla de la Consola
 
         opcion = mostrarOpcionesReportes();
-        analizarOpcionReporte(opcion, volver);
+        analizarOpcionReporte(opcion, volver, vAero, tam1, vecDestinos);
 
         system("pause");
     }
@@ -100,19 +130,127 @@ int mostrarOpcionesReportes()
     return opc;
 }
 
-void analizarOpcionReporte(int opcion, bool& volver){
+void analizarOpcionReporte(int opcion, bool& volver, Aerolinea vAero[], int tam1, float vecDestinos[20]){
     switch(opcion)
     {
     case 1:
-        cout << "REPORTE 1" << endl;
+        puntoA(vAero, tam1);
         break;
     case 2:
-        cout << "REPORTE 2" << endl;
+        puntoB(vecDestinos);
         break;
     case 0:
         volver = true;
         break;
     default:
         cout << "Opción ingresada No válida..." << endl;
+    }
+}
+
+bool cargarAerolineas(Aerolinea vecAerolineas[], int tam){
+    cout << " - INICIO CARGA DE AEROLÍNEAS -" << endl;
+    for(int i = 0; i < tam; i++){
+        cout << "Ingrese Código de Aerolínea (1 a 15): ";
+        cin >> vecAerolineas[i].codAerolinea;
+        cout << "Ingrese Nombre: ";
+        cin.ignore();
+        getline(cin, vecAerolineas[i].nombre);
+
+        cout << "------------------------------" << endl;
+    }
+    cout << " - FIN CARGA DE AEROLÍNEAS -" << endl;
+
+    return true;
+}
+
+bool cargarVuelos(Vuelo vecVuelos[], int tam){
+    cout << " - INICIO CARGA DE VUELOS -" << endl;
+    for(int i = 0; i < tam; i++){
+        cout << "Ingrese Código de Vuelo (1 a 100): ";
+        cin >> vecVuelos[i].codVuelo;
+        cout << "Ingrese Código de Aerolínea (1 a 15): ";
+        cin >> vecVuelos[i].codAerolinea;
+        cout << "Costo del Pasaje: $ ";
+        cin >> vecVuelos[i].costoPasaje;
+        cout << "Destino (1 a 20): ";
+        cin >> vecVuelos[i].destino;
+
+        cout << "------------------------------" << endl;
+    }
+    cout << " - FIN CARGA DE VUELOS -" << endl;
+
+    return true;
+}
+
+bool procesarReservas(Aerolinea vAero[], int tam1, Vuelo vVuelo[], int tam2, float vecDestinos[20]){
+    /// Variables Lote de Proceso
+    int codVuelo, cantPasajes, tipoCliente, dia;
+
+    // Punto A
+    int indiceVuelo;
+
+    // Punto B
+    int indiceDestino;
+
+    cout << " - RESERVAS DE VIAJES -" << endl << endl;
+    cout << "Código de Vuelo: ";
+    cin >> codVuelo;
+
+    while(codVuelo != 0){
+        cout << "Cantidad de Pasajes: ";
+        cin >> cantPasajes;
+        cout << "Tipo Cliente (1 - Regular | 2 - VIP): ";
+        cin >> tipoCliente;
+        cout << "Día de la Reserva: ";
+        cin >> dia;
+
+        // Punto A
+        indiceVuelo = buscarIndiceVuelo(vVuelo, tam2, codVuelo);
+        contarVuelo(vAero, tam1, vVuelo[indiceVuelo].codAerolinea);
+
+        // Punto B
+        indiceDestino = vVuelo[indiceVuelo].destino;
+        vecDestinos[indiceDestino - 1] += cantPasajes * vVuelo[indiceVuelo].costoPasaje;
+
+        cout << "---------------------------" << endl;
+        cout << "Código de Vuelo: ";
+        cin >> codVuelo;
+    }
+
+    cout << " - FIN RESERVAS -" << endl;
+
+    return true;
+}
+
+int buscarIndiceVuelo(Vuelo vVuelo[], int tam, int codigo){
+    for(int i=0; i < tam; i ++){
+        if(codigo == vVuelo[i].codVuelo){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void contarVuelo(Aerolinea vAero[], int tam, int codigo){
+    for(int i=0; i < tam; i ++){
+        if(codigo == vAero[i].codAerolinea){
+            vAero[i].cantVuelos++;
+            //cout << vAero[i].cantVuelos << endl;
+        }
+    }
+}
+
+void puntoA(Aerolinea vAero[], int tam){
+    cout << "  - PUNTO A -" << endl;
+    for(int i=0; i < tam; i ++){
+        cout << "Código de Aerolínea: " << vAero[i].codAerolinea << ", Aerolinea: " << vAero[i].nombre << ", Cant. Vuelos: " << vAero[i].cantVuelos << endl;
+    }
+}
+
+void puntoB(float vecDestinos[20]){
+    cout << "  - PUNTO B -" << endl;
+    for(int i=0; i < 20; i ++){
+        cout << "Destino: " << i + 1 << ", Recaudación: $ " << vecDestinos[i] << endl;
     }
 }
--- Semana 13/ej_adicional_lotes_menu/funciones.h
+++ Semana 13/ej_adicional_lotes_menu/funciones.h
@@ -1,19 +1,45 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
-
-void menuPrincipal();
-int mostrarOpciones();
-void analizarOpcion(int, bool&);
-
-void menuReportes();
-int mostrarOpcionesReportes();
-void analizarOpcionReporte(int, bool&);
+#include <iostream>
+using namespace std;
 
 /// Structs
+struct Aerolinea{
+    int codAerolinea;
+    string nombre;
 
+    //PUNTO A
+    int cantVuelos = 0;
+};
+
+struct Vuelo{
+    int codVuelo;
+    int codAerolinea;
+    float costoPasaje;
+    int destino;
+};
+
+/// Menú Principal
+void menuPrincipal();
+int mostrarOpciones();
+void analizarOpcion(int, bool&, Aerolinea [], int, Vuelo [], int, bool&, bool&, bool&, float[]);
+
+/// Menú Reportes
+void menuReportes(Aerolinea [], int, float []);
+int mostrarOpcionesReportes();
+void analizarOpcionReporte(int, bool&, Aerolinea[], int, float[]);
 
 /// Funciones de Carga
-//void cargarAerolineas(Aerolinea, int);
-//void cargarVuelos(Vuelo, int);
+bool cargarAerolineas(Aerolinea [], int);
+bool cargarVuelos(Vuelo [], int);
+
+/// Funciones Proceso
+bool procesarReservas(Aerolinea [], int, Vuelo [], int, float []);
+int buscarIndiceVuelo(Vuelo [], int, int);
+void contarVuelo(Aerolinea [], int, int);
+
+/// Funciones Reportes
+void puntoA(Aerolinea [], int);
+void puntoB(float []);
 
 #endif // FUNCIONES_H_INCLUDED
